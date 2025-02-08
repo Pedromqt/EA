26cb77c7a1fbef2506246b5dc0a2a8d6f51d3bdb
Pedro <pedrito4315@gmail.com>
2025-02-08 14:58:37 +0000

    D done

--- Exercises/Practice/D.cpp
+++ Exercises/Practice/D.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
-#include <string.h>
+#include <list>
+#include <string>
 
 int main(){
-    std::ios_base::sync_with_stdio(0);
-    std::cin.tie(0);
 
+    std::list<int> l = {0}; 
+    std::list<int>::iterator ptr = l.begin();
+    std::string cmd,dir;
+    int val;
+
+    while(std::cin>>cmd){
+        if(cmd == "MOVE"){
+            std::cin>>dir;
+            if(dir=="LEFT" && ptr != l.begin()){
+                ptr--;
+            }else if(dir=="RIGHT" && ptr != l.end()){
+                ptr++;
+            }
+        }else if(cmd == "INSERT"){
+            std::cin>>dir>>val;
+            if(dir=="LEFT"){
+                l.insert(ptr,val);
+            }else if (dir=="RIGHT"){
+                l.insert(std::next(ptr),val);
+            }
+        }
+    }
+    for(int v:l){
+        std::cout << v << "\n";
+    }
 
     return 0;
-}
+}   
