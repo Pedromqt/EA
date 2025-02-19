87e2561c291ab1ff7e3c2a9bbc536bfba7993ced
Pedro <pedrito4315@gmail.com>
2025-02-19 17:12:13 +0000

    fixed B

--- Exercises/Avaliation/B.cpp
+++ Exercises/Avaliation/B.cpp
@@ -1,41 +1,39 @@
 #include <iostream>
 #include <set>
 
-int count_moves(int n_moves,int x, int y){
+void count_moves(int n_moves,int x, int y,std::set<std::pair<int, int>>& distinct_positions){
     int moves[8][2] = {{-2,1},{-1,2},{1,2},{2,1},{2,-1},{1,-2},{-1,-2},{-2,-1}};
-    // fazer a coordenada de origem
     std::set<std::pair<int, int>> positions;
     positions.insert({x,y});
 
     for(int i=0;i<n_moves;i++){
         std::set<std::pair<int,int>> new_pos;
         for(auto pos : positions){
-            //coordenadas x y
-            int x = pos.first, y = pos.second;
-
+            int x = pos.first;
+            int y = pos.second;
             for(int j=0;j<=7;j++) {
                 int aux_x = x + moves[j][0];
                 int aux_y = y + moves[j][1];
                 if ((aux_x >= -200 && aux_x <= 200) && (aux_y >= -200 && aux_y <= 200)) {
                     new_pos.insert({aux_x, aux_y});
+                    distinct_positions.insert({aux_x, aux_y});
                 }
             }
             
         }
         positions = new_pos;
     }
-    return positions.size();
 }
 
 int main(){
-    int n,total_moves,x,y,possible_moves;
+    int n,x,y,possible_moves;
     std::cin >> n;
-    total_moves=0;
 
+    std::set<std::pair<int, int>> distinct_positions;
     for(int i=0;i<n;i++){
         std::cin >> x >> y >> possible_moves;
-        total_moves+=count_moves(possible_moves,x,y);
+        count_moves(possible_moves,x,y,distinct_positions);
     }
-    std::cout << total_moves << "\n";
+    std::cout << distinct_positions.size() << "\n";
     return 0;
 }
